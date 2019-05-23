5b270013ce46e8444f617aef40ccb8f449398a75
Young Jung <jungyoungjoon@gmail.com>
2019-05-23 00:09:49 -0400

    Refined capybara matcher in spec, spec passing

--- spec/features/articles/user_can_save_article_spec.rb
+++ spec/features/articles/user_can_save_article_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Registered User' do
   it 'can search for popular articles and save them' do
     VCR.use_cassette("features/articles") do
-      user = User.create(username: "test", password_digest: "test")
+      user = User.create!(username: "test", password: "test")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit root_path
@@ -11,18 +11,12 @@ RSpec.describe 'Registered User' do
       click_on "Submit"
 
       expect(current_path).to eq(popular_path)
-      within("#article_0") do
+
+      within(:css, "tr#article_0") do
         click_on "Save"
       end
 
-      expect(current_path).to eq(user_path(user))
-
-
-
-
-
-
-
+      expect(current_path).to eq(user_saved_articles_path(user))
     end
   end
 end
