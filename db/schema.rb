a8d4a6d0aa8ef3d162fef97da580947e50c7ba4f
Young Jung <jungyoungjoon@gmail.com>
2019-05-24 10:42:48 -0400

    Migration created to add read column to saved_articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190517213951) do
+ActiveRecord::Schema.define(version: 20190524144140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20190517213951) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "read"
     t.index ["user_id"], name: "index_saved_articles_on_user_id"
   end
 
