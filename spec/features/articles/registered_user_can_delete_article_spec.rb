c79f1242241efdf56799b113da2416bbb2731001
Young Jung <jungyoungjoon@gmail.com>
2019-05-23 20:27:43 -0400

    Created feature spec saved_articles#destroy

--- spec/features/articles/registered_user_can_delete_article_spec.rb
+++ spec/features/articles/registered_user_can_delete_article_spec.rb
@@ -4,10 +4,16 @@ RSpec.describe "Registered User" do
   it "can access their own saved articles and delete them" do
     user = create(:user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-    user.saved_articles.create()
+    create(:saved_article, user: user)
+    create(:saved_article, user: user)
 
+    visit user_saved_articles_path(user)
 
+    expect(page).to have_css("table#saved tr", count: 2)
 
+    click_link "Delete", match: :first
 
+    expect(current_path).to eq(user_saved_articles_path(user))
+    expect(page).to have_css("table#saved tr", count: 1)
   end
 end
