5924bdedbdecd8771f9350038531901d9d272c3e
Young Jung <jungyoungjoon@gmail.com>
2019-05-25 18:23:08 -0400

    Removed savedarticles#index method and view, moved rendering to root_path

--- app/controllers/saved_articles_controller.rb
+++ app/controllers/saved_articles_controller.rb
@@ -1,30 +1,25 @@
 class SavedArticlesController < ApplicationController
   before_action :set_user, only: [:create, :index, :destroy, :update]
 
-  def index
-    @unread_saved_articles = @user.saved_articles.where(read: false)
-    @read_saved_articles = @user.saved_articles.where(read: true)
-  end
-
   def create
     @saved_article = @user.saved_articles.new(saved_article_params.merge(read: false))
     if @saved_article.save
-      redirect_to user_saved_articles_path(@user)
+      redirect_to root_path
     else
-      redirect_to popular_path
+      redirect_to root_path
     end
   end
 
   def destroy
     saved_article = SavedArticle.find(params[:id])
     saved_article.destroy
-    redirect_to user_saved_articles_path(@user)
+    redirect_to root_path
   end
 
   def update
     saved_article = SavedArticle.find(params[:id])
     saved_article.update(saved_article_params)
-    redirect_to user_saved_articles_path(@user)
+    redirect_to root_path
   end
 
 
