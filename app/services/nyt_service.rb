2f46355c1b96f01e399186df4306cd2376f1c5db
Young Jung <jungyoungjoon@gmail.com>
2019-05-24 22:18:32 -0400

    Created api request to NYT top articles

--- app/services/nyt_service.rb
+++ app/services/nyt_service.rb
@@ -1,19 +1,27 @@
 class NytService
-  attr_reader :days
-  def initialize(days)
-    @days = days
+  attr_reader :params
+  def initialize(params)
+    @params = params
     @conn = Faraday.new(url: "https://api.nytimes.com") do |faraday|
       faraday.params["api-key"] = ENV["nyt_api_key"]
       faraday.adapter Faraday.default_adapter
     end
   end
 
-  def self.get_articles(days)
-    new(days).get_articles
+  def self.get_popular_articles(params)
+    new(params).get_popular_articles
   end
 
-  def get_articles
-    get_url("/svc/mostpopular/v2/viewed/#{days}.json")
+  def get_popular_articles
+    get_url("/svc/mostpopular/v2/viewed/#{params}.json")
+  end
+
+  def self.get_top_articles(params)
+    new(params).get_top_articles
+  end
+
+  def get_top_articles
+    get_url("/svc/topstories/v2/#{params}.json")
   end
 
   def get_url(url)
