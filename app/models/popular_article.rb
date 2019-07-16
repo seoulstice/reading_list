2daf40a28fd9c7df83331ef4d9e65d22ddeb9f3d
Young Jung <jungyoungjoon@gmail.com>
2019-07-16 17:25:48 -0400

    Completed styling of index, popular#index, and top#index pages

--- app/models/popular_article.rb
+++ app/models/popular_article.rb
@@ -14,7 +14,7 @@ class PopularArticle
     @abstract = attributes[:abstract]
     @section = attributes[:section]
     @published_date = attributes[:published_date]
-    @photo = attributes[:media][0][:"media-metadata"][1][:url]
+    @photo = attributes[:media][0][:"media-metadata"][2][:url]
   end
 
   def self.find_popular(days)
