09ad395a4cba7ce014f90dcef823d1ebaa47777d
Young Jung <jungyoungjoon@gmail.com>
2019-08-25 14:52:58 -0400

    Fixed user registration test

--- spec/features/users/new_spec.rb
+++ spec/features/users/new_spec.rb
@@ -4,8 +4,10 @@ RSpec.describe "User registration form" do
   it "creates a new User" do
     visit root_path
 
-    click_on "Register"
+    click_on "Login"
+    expect(current_path).to eq(login_path)
 
+    click_on "Register"
     expect(current_path).to eq(new_user_path)
 
     fill_in "user[username]", with: "hungryhippo"
@@ -16,7 +18,8 @@ RSpec.describe "User registration form" do
     new_user = User.last
 
     expect(current_path).to eq(root_path)
-    expect(page).to have_content("Signed in as #{new_user.username}")
-    expect(page).to have_link "Log Out"
+    expect(page).to have_content("Welcome to Reading List!")
+    expect(page).to have_content("Logged in as hungryhippo")
+    expect(page).to have_link "Logout"
   end
 end
