f1f94bfe32c96ec9a23117e256d97640c63e48ce
Young Jung <jungyoungjoon@gmail.com>
2019-08-25 17:30:44 -0400

    Created model validation test and added validations to model

--- app/models/saved_article.rb
+++ app/models/saved_article.rb
@@ -1,4 +1,10 @@
 class SavedArticle < ApplicationRecord
   belongs_to :user
+  validates_presence_of :abstract,
+                        :authors,
+                        :photo,
+                        :title,
+                        :url,
+                        :user_id
 
 end
